2f74def0fb31264e608eed6fa67cc60aca837b7f
Guilherme de Aguiar <gpacianotto@gmail.com>
2020-07-03 16:03:43 -0300

    equalizacao funcionando

--- atividade4/src/atividade4/Atividade4.java
+++ atividade4/src/atividade4/Atividade4.java
@@ -28,42 +28,46 @@ public class Atividade4 {
         lixo = i.nextLine();
         //System.out.println(lixo);
         
-        int coluna = i.nextInt();
         int linha = i.nextInt();
+        int coluna = i.nextInt();
         
         
-        int[][] Matriz = new int[coluna][linha];
+        int[][] Matriz = new int[linha + 1][coluna + 1];
         
         
         lim = i.nextInt();
         
-        for(int x = 0; x<coluna; x++){
-            for(int y = 0; y<linha; y++)
+        for(int x = 0; x<linha; x++){
+            for(int y = 0; y<coluna; y++)
             {
                 Matriz[x][y] = i.nextInt();
             }
             
         }
-        //System.out.println("Colunas: "+coluna+" Linhas: "+linha);
+        System.out.println("Colunas: "+coluna+" Linhas: "+linha);
         return Matriz;
     }
     
     public static void salvarImagem(int[][] imagem, String nameFile){
+        
+        int coluna = imagem[0].length - 1;
+        int linha = imagem.length - 1;
+        
         try {
         FileWriter fw = new FileWriter(nameFile);
         fw.write("P2");
         fw.write("\n");
-        fw.write(Integer.toString(imagem.length));
+        fw.write(Integer.toString(linha));
         fw.write(" ");
-        fw.write(Integer.toString(imagem[0].length));
+        fw.write(Integer.toString(coluna));
         fw.write("\n");
         fw.write(Integer.toString(lim));
         fw.write("\n");
-        for (int i = 0; i < imagem.length; i++) {
-            for (int j = 0; j < imagem[i].length; j++) {
+        for (int i = 0; i < linha; i++) {
+            for (int j = 0; j < coluna; j++) {
                     fw.write(imagem[i][j] + " ");
             }
-            fw.write("\n");
+            //fw.write("\n");
         }
         fw.flush();
         } catch (IOException e) {}
@@ -71,8 +75,8 @@ public class Atividade4 {
     
     public static int[] calcularHistograma(int matriz[][]) {
         int histograma[] = new int[lim + 1];
-        int coluna = matriz.length - 1;
-        int linha = matriz[0].length - 1;
+        int coluna = matriz[0].length - 1;
+        int linha = matriz.length - 1;
 
         for (int i = 0; i < linha; i++) {
             for (int j = 0; j < coluna; j++) {
@@ -85,6 +89,35 @@ public class Atividade4 {
     }
     
     
+    public static int[][] equalizarHistograma(int img[][], int[] histograma) {
+        
+        int coluna = img[0].length - 1;
+        int linha = img.length - 1;
+        
+        int mn = linha * coluna;
+        float[] pr = new float[lim + 1];
+        int[] sk = new int[lim + 1];
+
+        for (int i = 0; i < pr.length; i++) {
+            pr[i] = ((float) histograma[i]) / mn;
+        }
+
+        float acumulado = 0.0f;
+        for (int i = 0; i < sk.length; i++) {
+            acumulado += pr[i];
+            sk[i] = (int) Math.round(lim * acumulado);
+        }
+        
+        
+        for (int i = 0; i < linha; i++) {
+            for (int j = 0; j < coluna; j++) {
+                img[i][j] = sk[img[i][j]];
+            }
+        }
+
+        return img;
+    }
+    
     /**
      * @param args the command line arguments
      */
@@ -94,15 +127,24 @@ public class Atividade4 {
         Scanner ler = new Scanner(System.in);
 
         int[][] imagem;
-        int[][] fatiamento1;
-        int[][] fatiamento2;
-        int[][] transGamma;
-        int[][] flip;
+        int[] histograma;
+        int[][] imagemEqualizada;
         double gamma;
         
         imagem = criaMatriz(in);
         
+        salvarImagem(imagem, "copia.pgm");
         
+        histograma = calcularHistograma(imagem);
+        System.out.println("Histograma: ");
+        for(int i = 0; i <histograma.length; i++)
+        {
+            System.out.println(histograma[i]+" ");
+        }
+        
+        imagemEqualizada = equalizarHistograma(imagem, histograma);
+        
+        salvarImagem(imagemEqualizada, "equalizada.pgm");
         
         
         
